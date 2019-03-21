605a847fc42fdea0458f9fcdbf37935a28f9bfd1
chidimma Ezekwe <julietezekwe@gmail.com>
2019-03-21 12:55:04 +0100

    Blog pages app with ruby on rails

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,38 @@
+class PagesController < ApplicationController
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+    def index
+        @pages = Page.all
+    end
+
+    def show
+    rescue ActiveRecord::RecordNotFound
+        flash[:notice] = "Not found"
+        redirect_to action: :index
+    end
+    def new
+        @page = Page.new
+    end
+    def create
+     @page = Page.new(page_params)
+     @page.save
+     redirect_to @page
+    end
+
+    def edit
+    end
+    def update
+        @page.update(page_params)
+        redirect_to @page
+    end
+    def destroy
+        @page.destroy
+        redirect_to pages_path
+    end
+    private
+       def set_page
+        @page = Page.find(params[:id])
+       end
+        def page_params
+            params.require(:page).permit(:title, :body, :slug)
+        end
+end
